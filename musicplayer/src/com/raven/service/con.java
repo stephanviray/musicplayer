cb323b3b4b849ad8f7cb61d2d0e92e7d2af5f09a
stephanviray <120445241+stephanviray@users.noreply.github.com>
2024-01-17 11:05:21 +0800

    Add files via upload

---
+++ musicplayer/src/com/raven/service/con.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.raven.service;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author User
+ */
+class con {
+
+    static PreparedStatement prepareStatement(String select_UserID_UserName_Email_from_user_wh) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static PreparedStatement prepareStatement(String insert_into_user_UserName_Email_Password_, int RETURN_GENERATED_KEYS) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
